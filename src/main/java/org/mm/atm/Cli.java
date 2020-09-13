1b075730aea438e32e00ed0d641d556a5b07b3b1
Vlad Mamaev <st011079@student.spbu.ru>
2020-09-13 18:22:08 +0300

    add proper handling for floating point numbers input

--- src/main/java/org/mm/atm/Cli.java
+++ src/main/java/org/mm/atm/Cli.java
@@ -1,60 +1,103 @@
 package org.mm.atm;
 
-import java.util.Arrays;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Optional;
 import java.util.Scanner;
 
 class Cli {
   private long[] denominations;
   private long value;
+  private final Scanner scanner;
+  private final PrintStream out;
+
+  public Cli() {
+    this(System.in, System.out);
+  }
+
+  public Cli(InputStream in, PrintStream out) {
+    this.scanner = new Scanner(in);
+    this.out = out;
+  }
 
   public Cli start() {
-    var scanner = new Scanner(System.in);
+    readDenominations();
+    readValue();
+    return this;
+  }
 
-
+  private void readDenominations() {
     boolean validDenominations;
+    long[] denominations;
     do {
       validDenominations = true;
-      System.out.println("Enter comma-separated available denomination: ");
-      {
-        try {
-          denominations = Arrays.stream(scanner.nextLine().split(","))
-              .map(String::trim)
-              .mapToLong((Long::parseLong))
-              .toArray();
-        } catch (NumberFormatException e) {
-          System.out.println("Bad format.");
-          validDenominations = false;
-          continue;
-        }
+      out.println("Enter comma-separated available denomination: ");
+      final var split = scanner.nextLine().split(",");
+      denominations = new long[split.length];
 
-        if (Arrays.stream(denominations).anyMatch(l -> l <= 0)) {
-          System.out.println("Not positive numbers are not allowed.");
+      for (int i = 0, splitLength = split.length; i < splitLength; i++) {
+
+        final var parsed = safeParseLong(split[i].trim());
+        if (parsed.isEmpty()) {
           validDenominations = false;
+          break;
+        } else {
+          final var denomination = parsed.get();
+          if (denomination <= 0) {
+            out.println("Not positive numbers are not allowed.");
+            validDenominations = false;
+            break;
+          } else {
+            denominations[i] = denomination;
+          }
         }
       }
     } while (!validDenominations);
+    this.denominations = denominations;
+  }
 
+  private void readValue() {
     boolean validValue;
-
+    Optional<Long> parsed;
     do {
       validValue = true;
-      System.out.println("Enter value to exchange: ");
-      try {
-        final var line = scanner.nextLine().trim();
-        value = Long.parseLong(line);
-      } catch (NumberFormatException e) {
+      out.println("Enter value to exchange: ");
+      final var line = scanner.nextLine().trim();
+
+      parsed = safeParseLong(line);
+      if (parsed.isEmpty()) {
         validValue = false;
-        System.out.println("Bad input.");
-        continue;
+      } else {
+        final var value = parsed.get();
+        if (value < 0) {
+          validValue = false;
+          out.println("Negative number is not allowed.");
+        }
       }
 
-      if (value < 0) {
-        validValue = false;
-        System.out.println("Negative number is not allowed.");
-      }
     } while (!validValue);
+    this.value = parsed.get();
+  }
 
-    return this;
+  private Optional<Long> safeParseLong(String line) {
+    try {
+      final var parsedDouble = Double.parseDouble(line);
+      if (!Double.isFinite(parsedDouble)) {
+        out.printf("\"%s\" is not a finite number.\n", line);
+        return Optional.empty();
+      }
+
+      if (parsedDouble % 1 != 0) {
+        out.printf("Decimal part must be zero: \"%s\".\n", line);
+        return Optional.empty();
+      }
+      return Optional.of((long) parsedDouble);
+
+    } catch (NumberFormatException nfe) {
+      out.printf("Provided string is not a number: \"%s\".\n", line);
+      return Optional.empty();
+    }
+
   }
 
   public long[] getDenominations() {
