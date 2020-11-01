191b1bfb3839f51f7dfce81e788d22e61bf12bb8
Vlad Mamaev <st011079@student.spbu.ru>
2020-11-01 13:48:10 +0300

    add increment\decrement maxSize for LimitedSizeCollection

--- src/main/java/org/mm/pool/common/LimitedSizeCollection.java
+++ src/main/java/org/mm/pool/common/LimitedSizeCollection.java
@@ -7,19 +7,25 @@ import java.util.stream.Stream;
 public class LimitedSizeCollection<T> {
   private final AtomicInteger currentSize;
   private final ConcurrentLinkedQueue<T> queue;
-  //todo increase\decrease
-  private volatile int maxSize;
+  private final AtomicInteger maxSize;
 
   public LimitedSizeCollection(int maxSize) {
-    this.maxSize = maxSize;
+    checkMaxSize(maxSize);
+    this.maxSize = new AtomicInteger(maxSize);
     this.queue = new ConcurrentLinkedQueue<>();
     this.currentSize = new AtomicInteger(0);
+  }
+
+  private void checkMaxSize(int maxSize) {
+    if (maxSize < 0) {
+      throw new IllegalArgumentException("Max size can't be negative");
+    }
   }
 
   public boolean add(T object) {
     do {
       int currentSize = this.currentSize.get();
-      if (currentSize >= maxSize) {
+      if (currentSize >= maxSize.get()) {
         return false;
       }
       final var added = queue.add(object);
@@ -27,7 +33,7 @@ public class LimitedSizeCollection<T> {
         return false;
       } else {
         if (this.currentSize.compareAndSet(currentSize, currentSize + 1)) {
-          if (this.currentSize.get() <= maxSize) {
+          if (this.currentSize.get() <= maxSize.get()) {
             return true;
           } else {
             this.queue.remove(object);
@@ -43,6 +49,26 @@ public class LimitedSizeCollection<T> {
   public void remove(T object) {
     queue.remove(object);
     currentSize.getAndDecrement();
+  }
+
+  public void incrementMaxSize(int increment) {
+    int currentMaxSize;
+    int newMaxSize;
+    do {
+      currentMaxSize = this.maxSize.get();
+      newMaxSize = currentMaxSize + increment;
+    } while (!this.maxSize.compareAndSet(currentMaxSize, newMaxSize));
+  }
+
+
+  public void decrementMaxSize(int decrement) throws IllegalArgumentException {
+    int currentMaxSize;
+    int newMaxSize;
+    do {
+      currentMaxSize = this.maxSize.get();
+      newMaxSize = currentMaxSize - decrement;
+      checkMaxSize(newMaxSize);
+    } while (!this.maxSize.compareAndSet(currentMaxSize, newMaxSize));
   }
 
   public Stream<T> stream() {
