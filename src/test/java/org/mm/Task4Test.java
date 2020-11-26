b7ea90169f4dc3192aeb6ab4f4add0a6e9d55ae8
Vlad Mamaev <st011079@student.spbu.ru>
2020-11-26 10:47:13 +0300

    fix format

--- src/test/java/org/mm/Task4Test.java
+++ src/test/java/org/mm/Task4Test.java
@@ -29,21 +29,19 @@ class Task4Test {
 
   @AfterEach
   void cleanUp() throws IOException {
-    if (!Files.exists(tempDir))
+    if (!Files.exists(tempDir)) {
       return;
+    }
     Files.walkFileTree(tempDir,
         new SimpleFileVisitor<>() {
           @Override
-          public FileVisitResult postVisitDirectory(
-              Path dir, IOException exc) throws IOException {
+          public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
             Files.delete(dir);
             return FileVisitResult.CONTINUE;
           }
 
           @Override
-          public FileVisitResult visitFile(
-              Path file, BasicFileAttributes attrs)
-              throws IOException {
+          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
             Files.delete(file);
             return FileVisitResult.CONTINUE;
           }
